7d458410f5684a85a051244e05591f55da92b653
Lajaynees <lscingram@yahoo.com>
2021-11-09 09:59:24 -0500

    added array questions

--- script.js
+++ script.js
@@ -33,3 +33,30 @@ const goBackBtn = document.querySelector("#goback");
 const clearScrBtn = document.querySelector("#clearscores");
 const viewScrBtn = document.querySelector("#view-scores");
 
+// Object/array for question, answer, true/false
+const questions = [ 
+    {
+       
+        question: "What is callback function in JavaScript?",
+        answers: ["1. strings", "2. a function passed to another function,", "3. alerts", "4. numbers"],
+        correctAnswer: "2"
+    },
+    {
+        
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["1. <js>", "2. <javascript>", "3. <scripting>", "4. <script>"],
+        answer: "d. <script>"
+    },
+    {
+        
+        question: "Arrays in Javascript can be used to store ____.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    }
+];
