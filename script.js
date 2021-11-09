8418f1ef5402ab841574744155cae0848d8d1a52
Lajaynees <lscingram@yahoo.com>
2021-11-09 09:04:59 -0500

    varibles added for question sec and scores

---
+++ script.js
@@ -0,0 +1,23 @@
+// variables EL
+let timeEl = document.querySelector("p.time");
+let secondsLeft = 100;
+let scoreEl = document.querySelector("#score");
+//^time-score
+
+//sections
+//intro
+const introEl = document.querySelector("#intro");
+
+//questions-where and how many
+const questionsEl = document.querySelector("#questions");
+let questionEl = document.querySelector("#question");
+let questionCount = 0;
+const yesnoEl = document.querySelector("#yesno");
+
+// end sections-scores
+const finalEl = document.querySelector("#final");
+let initialsInput = document.querySelector("#initials");
+const highscoresEl = document.querySelector("#highscores");
+let scoreListEl = document.querySelector("#score-list");
+let scoreList = [];
+
